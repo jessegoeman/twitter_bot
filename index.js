9a40799159d1838a3fb022b3df675322af10c95f
DELCLEF Wietse [ANTWERPSE BOUWWERKEN] <wdelclef@YELN.EBX.CORP>
2020-08-13 22:01:24 +0200

    test

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 require('dotenv').config()
 const Twit = require('twit');
 const T = new Twit({
-  consumer_key: process.env.APPLICATION_CONSUMER_KEY,
-  consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.APPLICATION_CONSUMER_KEY,
+    consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 // start stream and track tweets
-const stream = T.stream('statuses/filter', {follow: ['3372594015']});   //ID van user account
+const stream = T.stream('statuses/filter', {
+    follow: ['3372594015']  //ID's van users
+});
 
 // use this to log errors from requests
-function responseCallback (err, data, response) {
- console.log(err);
+function responseCallback(err, data, response) {
+    console.log(err);
 }
 
 // event handler
 stream.on('tweet', tweet => {
+    console.log(tweet)
     var res = {
         status: 'test response',
         in_reply_to_status_id: '' + tweet.id_str
-      };
-  T.post('statuses/update', res, responseCallback);
-  // like
-  T.post('favorites/create', {id: tweet.id_str}, responseCallback);
+    };
+    T.post('statuses/update', res, responseCallback);
+    // like
+    T.post('favorites/create', {
+        id: tweet.id_str
+    }, responseCallback);
 });
